13e2a2dd5b0d432e67bf0b462d0343d905381cb7
Esouder <eric@souder.ca>
2022-07-16 00:33:56 -0700

    added basic trajectory planning template (no algo yet)

--- lib/dataStore/stateData.cpp
+++ lib/dataStore/stateData.cpp
@@ -3,5 +3,5 @@
 
 StateMachine::StateEnum* StateData::state;
 
-int8_t StateData::driveSpeed;
+int8_t StateData::driveSpeed; //should we have setters and getters for these sorts of things?
 int8_t StateData::driveSteer;
