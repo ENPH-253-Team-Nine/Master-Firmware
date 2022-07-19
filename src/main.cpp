e66540ae0f809439a202f2c334f5e8a0ca17f5c0
Esouder <eric@souder.ca>
2022-07-19 02:53:32 -0700

    Sensorssssss

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <motorControl.h>
 #include <stateData.h>
 #include <trajectoryPlanning.h>
+#include <sensors.h>
 
 StateMachine::StateManager *stateManager = new StateMachine::StateManager();
 
@@ -14,10 +15,13 @@ motors::MotorManager *motorManager = new motors::MotorManager();
 
 trajectory::TrajectoryManager *trajectoryManager = new trajectory::TrajectoryManager();
 
+sensors::SensorManager *sensorManager = new sensors::SensorManager();
+
 void setup()
 {
   lightManager->setup();
   motorManager->setup();
+  sensorManager->setup();
 }
 
 void loop()
@@ -26,6 +30,7 @@ void loop()
   lightManager->poll();
   motorManager->poll();
   trajectoryManager->poll();
+  sensorManager->setup();
   
   // below code good for testing motor control. Will be removed in a PR or two.
   /*if(*StateData::state == StateMachine::StateEnum::Error){
