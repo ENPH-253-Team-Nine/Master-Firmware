219cf1e56b7ea07e3367b6a50f171a0b0e03961e
Esouder <eric@souder.ca>
2022-08-11 04:47:32 -0700

    servo shit

--- lib/servoControl/servoControl.cpp
+++ lib/servoControl/servoControl.cpp
@@ -2,6 +2,11 @@
 
 using namespace servos;
 
+int map(int value){
+    return 5+(double)value*(double)7/((double)180);
+
+}
+
 ServoManager::ServoManager()
 {
     elbowServo = new Servo();
@@ -15,15 +20,17 @@ ServoManager::~ServoManager()
 void ServoManager::setup()
 {
 
-    elbowServo->attach(PA9);
-    clawServo->attach(PA10);
+    //elbowServo->attach(PA9);
+    //clawServo->attach(PA10);
 }
 
 void ServoManager::poll()
 {
     // right now this only controls servo angle and not speed.
     // we can control speed but it will be some more work and more code.
-
-    elbowServo->write(StateData::elbowServoPos);
-    clawServo->write(StateData::clawServoPos);
+    int clawServoValue;
+    pwm_start(PA_9,20,map(StateData::clawServoPos)*4.095,RESOLUTION_12B_COMPARE_FORMAT);
+    pwm_start(PA_10,20,map(StateData::elbowServoPos)*4.095,RESOLUTION_12B_COMPARE_FORMAT);
+    //elbowServo->write(StateData::elbowServoPos);
+    //clawServo->write(StateData::clawServoPos);
 }
